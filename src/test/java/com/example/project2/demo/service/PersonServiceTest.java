ee777716a687d2db95670678d4f55c8e06be5c7a
Taek <01077275247a@gmail.com>
2020-02-14 00:26:39 +0900

    Service modified #2 using mockito

--- src/test/java/com/example/project2/demo/service/PersonServiceTest.java
+++ src/test/java/com/example/project2/demo/service/PersonServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.project2.demo.service;
 
+import com.example.project2.demo.controller.dto.PersonDto;
 import com.example.project2.demo.domain.Person;
 import com.example.project2.demo.repository.PersonRepository;
 import org.assertj.core.util.Lists;
+import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,10 +13,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static com.sun.tools.doclint.Entity.times;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -35,4 +41,28 @@ class PersonServiceTest {
 
     }
 
+    @Test
+    void getPerson() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        Person person = personService.getPerson(1L);
+        assertThat(person.getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPersonIfNotFound() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        Person person = personService.getPerson(1L);
+        assertThat(person).isNull();
+    }
+
+    @Test
+    void put() {
+        PersonDto personDto = PersonDto.of("martin", "programming", "판교", LocalDate.now(),"programmer", "010-1111-2222");
+        personService.put(personDto);
+        verify(personRepository,times(1)).save(any(Person.class));
+    }
 }
